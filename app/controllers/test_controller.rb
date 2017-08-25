c6845c4db393aac6acb99f0968de580091909ef8
rafaSalgado <rasalgadol@unal.edu.co>
2017-08-25 12:58:12 +0000

    scaffold

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,5 @@
+class TestController < ApplicationController
+    def quien
+        render html:"soy tu padre"
+    end
+end
--- app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-    
-    def pru
-        render html: "soy tu padre"
-    end
-end
